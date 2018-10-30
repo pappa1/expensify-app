2a91aa30e6a1b27495ce869ed120ad6f00422df3
Robert <dykej.robert@gmail.com>
2018-10-30 15:17:02 +0700

    Add Expensify styles

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -26,20 +26,29 @@ export class ExpenseListFilters extends React.Component {
     };
     render() {
     return (
-        <div>
-          <input 
-           type="text"
-           value={this.props.filters.text}
-           onChange={this.onTextChange}
-          />
-          <select
-           value={this.props.filters.sortBy}
-           onChange={this.onSortChange}
+        <div className="content-container">
+          <div className="input-group">
+            <div className="input-group__item">
+            <input
+              className="text-input"
+              type="text"
+              placeholder="Search expenses"
+              value={this.props.filters.text}
+              onChange={this.onTextChange}
+             />
+           </div>
+            <div className="input-group__item">
+            <select
+             className="select"
+             value={this.props.filters.sortBy}
+             onChange={this.onSortChange}
           >
             <option value="date">Date</option>
             <option value="amount">Amount</option>
           </select>
-          <DateRangePicker 
+            </div>
+            <div className="input-group__item">
+            <DateRangePicker 
             startDate={this.props.filters.startDate}
             endDate={this.props.filters.endDate}
             onDatesChange={this.onDatesChange}
@@ -49,7 +58,12 @@ export class ExpenseListFilters extends React.Component {
             numberOfMonths={1}
             isOutsideRange={() => false}
           />
-        </div>
+            </div>
+            </div>
+          </div>
+          
+          
+          
     )
 }
 };
